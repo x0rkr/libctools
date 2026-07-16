df052182c3f1cea89061c9aeb5109fa621f6b8a7
x0rkr <missionjee250@gmail.com>
2026-07-16 20:34:57 +0530

    build(stdc): add generic hash table prototypes and update Makefile

--- stdc/include/libctools_std.h
+++ stdc/include/libctools_std.h
@@ -14,7 +14,7 @@ void println(const char *format, ...);
 //strreplace()
 char *strreplace(const char *str, const char *old_sub, const char *new_sub);
 
-// --- Generic Vector Module ---
+//Generic Vector Module 
 #define VECTOR_INIT_CAPACITY 4
 
 typedef struct {
@@ -29,5 +29,24 @@ void vector_free(vector_t *v);
 bool vector_push_back(vector_t *v, const void *element);
 void *vector_get(const vector_t *v, size_t index);
 bool vector_set(vector_t *v, size_t index, const void *element);
+
+//Hash table
+typedef struct ht_node {
+    char *key;
+    void *value;
+    struct ht_node *next;
+} ht_node_t;
+
+typedef struct {
+    ht_node_t **buckets;
+    size_t size;
+    size_t count;
+} hash_table_t;
+
+// Hash Table API
+hash_table_t *ht_create(size_t initial_size);
+bool ht_insert(hash_table_t *ht, const char *key, void *value);
+void *ht_get(hash_table_t *ht, const char *key);
+void ht_destroy(hash_table_t *ht);
 
 #endif /* LIBCTOOLS_STD_H */
